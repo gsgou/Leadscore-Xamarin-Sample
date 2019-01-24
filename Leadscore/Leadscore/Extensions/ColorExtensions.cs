6b6ee5e46440f96e2dd922795235e25bd74134b9
Giorgos Sgouridis <giorgos@live.de>
2019-01-24 23:41:36 +0100

    Added some Material and Leadscore colors for branding

---
+++ Leadscore/Leadscore/Extensions/ColorExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+
+namespace Leadscore.Extensions
+{
+    public static class ColorExtensions
+    {
+        #region Material Colors
+
+        // Background Colors
+        public static readonly Color BackgroundColorLight = Color.FromHex("#FAFAFA");
+
+        // Dividers Color
+        public static readonly Color TextColorDarkDividers = Color.FromRgba(0, 0, 0, 0.12);
+
+        // Text Colors
+        // Dark
+        public static readonly Color TextColorDarkPrimary = Color.FromRgba(0, 0, 0, 0.87);
+        // Light
+        public static readonly Color TextColorLightPrimary = Color.FromHex("#FFFFFF");
+        // Error
+        // Color: 100% red (A400)
+        public static readonly Color TextColorError = Color.FromHex("#FF1744");
+
+        #endregion
+
+        #region Leadscore Colors
+
+        public static readonly Color LeadscorePrimary = Color.FromHex("#1262BE");
+
+        #endregion
+    }
+}
