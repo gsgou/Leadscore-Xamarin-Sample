9e8acf7aa68fbac84042d8cb6fdb750bef6c4e76
Giorgos Sgouridis <giorgos@live.de>
2019-01-24 22:31:46 +0100

    Added logo_with_brand_name in LoginPage and the rquired ImageEmbeddedResourceExtension

---
+++ Leadscore/Leadscore/Extensions/ImageEmbeddedResourceExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace Leadscore.Extensions
+{
+    // You exclude the 'Extension' suffix when using in Xaml markup
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+    public class ImageEmbeddedResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            return ImageSource.FromResource(Source);
+        }
+    }
+}
