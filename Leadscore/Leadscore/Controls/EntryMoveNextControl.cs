3493033dac94128a518263d7b468934bc9a9532e
Giorgos Sgouridis <giorgos@live.de>
2019-01-25 03:38:47 +0100

    Implemented EntryMoveNextControl for the LoginPage

---
+++ Leadscore/Leadscore/Controls/EntryMoveNextControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+
+using Xamarin.Forms;
+
+namespace Leadscore.Controls
+{
+    public class EntryMoveNextControl : Entry
+    {
+        public static readonly BindableProperty NextEntryProperty = BindableProperty.Create(
+            nameof(NextEntry),
+            typeof(View),
+            typeof(Entry)
+            );
+            
+        public View NextEntry
+        {
+            get => (View)GetValue(NextEntryProperty);
+            set => SetValue(NextEntryProperty, value);
+        }
+
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            this.Completed += (sender, e) =>
+            {
+                this.OnNext();
+            };
+        }
+
+        void OnNext()
+        {
+            NextEntry?.Focus();
+        }
+    }
+}
