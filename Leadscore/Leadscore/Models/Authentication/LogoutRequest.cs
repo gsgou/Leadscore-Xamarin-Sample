6536e0a6c9666ccd2497428fc9360a6725eea12e
Giorgos Sgouridis <giorgos@live.de>
2019-01-28 10:27:30 +0100

    Added LogoutRequest in Authentication Models

---
+++ Leadscore/Leadscore/Models/Authentication/LogoutRequest.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Leadscore.Models
+{
+    public class LogoutRequest
+    {
+        [JsonProperty("authToken", NullValueHandling = NullValueHandling.Ignore)]
+        public string AuthToken { get; set; }
+    }
+}
