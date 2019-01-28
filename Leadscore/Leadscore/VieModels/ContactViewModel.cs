ce51ec46ab6bd90b2c4e5a138aeddb28167e81e7
Giorgos Sgouridis <giorgos@live.de>
2019-01-28 17:44:23 +0100

    Added ContactViewModel to bind it later in ContactsPage

---
+++ Leadscore/Leadscore/VieModels/ContactViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+using ReactiveUI.Fody.Helpers;
+
+using Leadscore.Models;
+
+namespace Leadscore.ViewModels
+{
+    public class ContactViewModel : BasePageViewModel
+    {
+        [Reactive] public string DisplayName { get; private set; }
+
+        [Reactive] public string Email { get; private set; }
+
+        [Reactive] public string Birthday { get; private set; }
+
+        public bool TrySet(Contact result)
+        {
+            var response = false;
+
+            if (result == null)
+            {
+                return response;
+            }
+
+            try
+            {
+                response = true;
+
+                DisplayName = string.Format("{0} {1} {2}", result.FirstName, result.MiddleName, result.LastName);
+
+                Email = result.Emails
+                              .FirstOrDefault(em => em.Primary == true)
+                              .EmailEmail;
+
+                Birthday = result.Birthday;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+
+            return response;
+        }
+    }
+}
