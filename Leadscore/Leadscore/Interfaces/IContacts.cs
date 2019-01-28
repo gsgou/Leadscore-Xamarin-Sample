8f37b10570cc98f76db8adcda912f84f63370fb0
Giorgos Sgouridis <giorgos@live.de>
2019-01-29 00:59:06 +0100

    Added IContacts and and its implementation with Refit

---
+++ Leadscore/Leadscore/Interfaces/IContacts.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Leadscore.Models;
+using Refit;
+
+namespace Leadscore.Interfaces
+{
+    public interface IContacts
+    { 
+        [Headers("Content-Type: application/json", "Accept: application/json")]
+        [Post("/contacts/filter?offset=0&limit=100")]
+        Task<FindFilteredContactsResult> FindFilteredContacts([Body(BodySerializationMethod.Json)] FindFilteredContactsRequest request, [Header("authToken")] string authToken);
+    }
+}
---
+++ Leadscore/Leadscore/Services/ContactsService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Leadscore.Helpers;
+using Leadscore.Interfaces;
+using Leadscore.Models;
+using Refit;
+
+namespace Leadscore.Services
+{
+    public class ContactsService
+    {
+        readonly HttpClient _httpClient;
+        readonly HttpClientHandler _httpClientHandler;
+        readonly IContacts _restService;
+
+        const string ApiBaseUrl = "https://internal-api-staging-lb.interact.io/v2";
+
+        public ContactsService()
+        {
+            _httpClientHandler = new HttpClientHandler();
+            _httpClient = new HttpClient(_httpClientHandler)
+            {
+                BaseAddress = new Uri(ApiBaseUrl)
+            };
+
+            _restService = RestService.For<IContacts>(_httpClient);
+        }
+
+        public async Task<IEnumerable<Contact>> FindFilteredContact(string authToken)
+        {
+            return await Policies.RetryPolicy(async () =>
+            {
+                var filters = new Filter[] {
+                    new Filter { Field = "contactType", Op = "eq", Value = "COMPANY" },
+                    new Filter { Field = "contactType", Op = "eq", Value = "PERSON" }
+                }.AsEnumerable();
+
+                var request = new FindFilteredContactsRequest()
+                {
+                    DefaultOperator = "OR",
+                    Filters = filters
+                };
+
+                var result = (await _restService.FindFilteredContacts(request, authToken))?.Data;
+                return result;
+            });
+        }
+    }
+}
