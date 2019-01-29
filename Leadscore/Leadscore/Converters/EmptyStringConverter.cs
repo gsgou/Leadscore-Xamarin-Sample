cd07c5d8c93817182cd062385de6ddc92eda9b9b
Giorgos Sgouridis <giorgos@live.de>
2019-01-29 01:05:32 +0100

    Added the EmptyStringConverter in order to show "N/A" instead of empty values

---
+++ Leadscore/Leadscore/Converters/EmptyStringConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Leadscore.Converters
+{
+    public class EmptyStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = (String)value;
+            return String.IsNullOrWhiteSpace(str) ? "N/A" : str;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException(string.Format("{0} is one-way", nameof(EmptyStringConverter)));
+    }
+}
